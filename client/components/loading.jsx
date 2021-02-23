9eb0047eadc3312f6d6f788faefb6892bbe86144
Keaton13 <bajabuggy313@gmail.com>
2021-02-22 20:13:54 -0800

    changed className to class -_-

--- client/components/loading.jsx
+++ client/components/loading.jsx
@@ -6,7 +6,7 @@ class Loading extends React.Component {
     }
     render() {
         return (
-            <div className="lds-ring"><div></div><div></div><div></div><div></div></div>)
+            <div class="lds-ring"><div></div><div></div><div></div><div></div></div>)
     }
 }
 
