1b648445ffce8a5ca57441c090d5fa4427c884ab
Keaton13 <bajabuggy313@gmail.com>
2021-02-22 21:17:28 -0800

    Adding Fuji logo to loading Screen

--- client/components/loading.jsx
+++ client/components/loading.jsx
@@ -7,7 +7,7 @@ class Loading extends React.Component {
     }
     render() {
         return (
-            <div>
+            <div className="container">
                 <div className="row">
                     <div className="lds-ring"><div></div><div></div><div></div><div></div></div>
                     <h1 className="text-center fuji-Font float-left mt-2 ml-2">fuji</h1>
