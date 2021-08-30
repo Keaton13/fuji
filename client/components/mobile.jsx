1a14ee2dd58bc6de740656e86c31668431b5efae
Keaton13 <bajabuggy313@gmail.com>
2021-08-30 16:56:59 -0700

    Got background full width and height

--- client/components/mobile.jsx
+++ client/components/mobile.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 
 class MobileError extends React.Component {
-    render() {
-        return (
-                <div className="container m-auto backgroundImage2">
-                    <div className="row m-auto">
-                        <div className="col m-auto">
-                            <div class="modal-dialog modal-dialog-centered text-center">
-                                <h1>Please view app with a mobile device -_-</h1>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-        )
-    }
+  render() {
+    return (
+      <div className="container m-auto backgroundImage2 h-100">
+        <div className="row m-auto">
+          <div className="col m-auto">
+            <div className="modal-dialog modal-dialog-centered text-center Mg5">
+              <h1>Please view app with a mobile device -_-</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default MobileError;
